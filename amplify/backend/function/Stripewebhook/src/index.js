55083b6bd50949dff5694d53c8b159d934336ee4
Ali Razzaq <alirazzaquet@gmail.com>
2021-05-15 09:18:47 +0500

    post-payment events added

---
+++ amplify/backend/function/Stripewebhook/src/index.js
@@ -0,0 +1,35 @@
+
+
+exports.handler = async (event) => {
+    console.log(event)
+    const body = JSON.parse(event.body);
+
+      // Handle the event
+  switch (body.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = body.data.object;
+      // update database and set order to successful
+      console.log('PaymentIntent was successful!');
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = body.data.object;
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${body.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify({received: true}),
+    };
+    return response;
+};
